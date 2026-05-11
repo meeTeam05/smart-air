7975f6634c795dcabd963a8588ac40bd8a0cf2a8
nhat092005 <minhnhat092005@gmail.com>
2026-05-11 14:42:30 +0700

    chore(app): point default API to public domain

--- app/lib/core/env.dart
+++ app/lib/core/env.dart
@@ -3,6 +3,6 @@ class Env {
 
   static const apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://192.168.1.16/api',
+    defaultValue: 'https://minhnhat05.xyz/api',
   );
 }
